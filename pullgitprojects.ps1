d25aa8a371781345774edd84596c32e620f26f24
0xPrime <eivind.utnes@gmail.com>
2018-06-09 01:39:56 +0200

    Initial commit.

---
+++ pullgitprojects.ps1
@@ -0,0 +1,16 @@
+param 
+(
+    # Path to folder containing your projects 
+    [string]$path = (Resolve-Path ..\).path
+)
+$folders = Get-ChildItem -Directory $path
+
+Write-Host "> Git pulling projects in folder $path"
+
+foreach ($folder in $folders) 
+{
+    Write-Host "> Git Pulling project $folder.Name"
+    Set-Location ($path + "\" + $folder.Name)
+    git pull
+    #Set-Location ..
+} 
