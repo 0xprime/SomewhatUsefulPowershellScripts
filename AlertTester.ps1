a528f2a0f1aa34eb406174a5a8d7eb37abfad7c8
0xPrime <eivind.utnes@gmail.com>
2018-06-17 01:37:16 +0200

    Cleaned up code.

--- AlertTester.ps1
+++ AlertTester.ps1
@@ -1,5 +1,3 @@
-# This still in testing. 
-
 Param(
     [Parameter(Mandatory=$true, Position=0)]
     [ValidateSet("smtp","teams","slack")]
@@ -20,37 +18,48 @@ $alertBody = "This is an alert test."
 
 switch ($alert){
     smtp {
-        if ((!$smtpFrom) -and (!$smtpTo) -and (!$smtpServer)) { Write-Warning "> Cannot send email, requires smtpTo, smtpFrom and smtpServer."; break}
+        if ((!$smtpFrom) -and (!$smtpTo) -and (!$smtpServer)) { 
+            Write-Warning "> Cannot send email, requires smtpTo, smtpFrom and smtpServer."
+            break 
+        }
         
         # $credentials = $null
         Write-Verbose "> Sending Email alert to $smtpTo from $smtpFrom through $smtpServer." 
         Send-Mailmessage -to $smtpTo -from $smtpFrom -subject $alertSubject -BodyAsHtml $alertBody -smtpserver $smtpServer #-Credential $credentials
     }
     teams {
-        # Requires Teams Webhook Connector.
-        if (!$webhook) {Write-Warning "> Cannot send alert, requires Webhook."; break}
-        $body = @{
+        # Requires Teams Webhook.
+        if (!$webhook) {
+            Write-Warning "> Cannot send alert, requires Webhook."
+            break
+        }
+        Write-Verbose "> Sending alert to Teams channel."
+        $data = @{
             'text'= "<p>" + $alertSubject + "</p><p>" + $alertBody + "</p>"
         }
         $request = @{
             Headers = @{'accept'='application/json'}
-            Body = $Body | convertto-json
+            Body = $data | ConvertTo-JSON
             Method = 'POST'
             URI = $webhook 
         }
         Invoke-RestMethod @request
     }
     slack {
-        # Todo
-        if (!$webhook) {Write-Warning "> Cannot send alert, requires Webhook."; break}
-        $body = @{
+        # Requires Slack webhook.
+        if (!$webhook) { 
+            Write-Warning "> Cannot send alert, requires Webhook."
+            break 
+        }
+        Write-Verbose "> Sending alert to Slack channel."
+        $data = @{
             'text'= $alertBody
             'pretext' = $alertSubject
             'color' = "warning"
         }
         $request = @{
             Headers = @{'accept'='application/json'}
-            Body = $Body | convertto-json
+            Body = $data | ConvertTo-JSON
             Method = 'POST'
             URI = $webhook 
         }
